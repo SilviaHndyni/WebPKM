6c80289e5321d752f6155f5e49812a0cf6c51e22
SilviaHndyni <silviahndyni@gmail.com>
2020-09-17 21:30:09 +0800

    layout awal

---
+++ home.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Banjar</title>
+  </head>
+  <body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+  <a class="navbar-brand" href="#">
+    <img src="img/logo.png" width="70" height="40" class="d-inline-block align-top" alt="" loading="lazy">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.php">Beranda <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="wisata.php">Destinasi Wisata</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link" href="makan.php">Tempat Makan</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link" href="penginapan.php">Penginapan</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link" href="#info">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- End Navbar -->
+
+  <div class="jumbotron mb-5">
+    <h1 class="display-4"><span class="font-weight-bold">Hello, world!</span></h1>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <a class="btn btn-primary btn-lg" href="#content" role="button">Go</a>
+  </div>
+
+<!-- Content -->
+  <div class="container-fluid" id="content">
+    <p class="text-center font-weight-bold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem sunt nisi voluptates facere enim quo? Sed saepe inventore sequi aliquid optio. Corrupti aspernatur totam ratione. Magnam animi facere illo magni!</p>
+    <hr class="my-4">
+    <div class="row">
+      <div class="col-lg-4">
+        <a href="wisata.php"><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+      </div>
+
+      <div class="col-lg-4">
+        <a href="makan.php"><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+      </div>
+
+      <div class="col-lg-4">
+        <a href="penginapan.php"><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-5 bg-dark" id="info">
+    <div class="row">
+      <div class="col-lg-6">
+        <img src="img/MenaraPandang.jpg" class="foto" alt="">
+      </div>
+
+      <div class="col-lg-6 p-5">
+      <h1 class="text-center mb-5 text-light"><span class="font-weight-bold">Apa itu Banjarmasin?</span></h1>
+        <p class="text-center font-weight-bold text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolorem placeat nihil vero nam, optio dignissimos iste nobis adipisci omnis ipsum deserunt, cupiditate magnam alias blanditiis? Molestias autem tenetur numquam!</p>
+      </div>
+    </div>
+  </div>
+<!-- End Content -->
+
+<!-- Footer -->
+  <footer class="page-footer font-small bg-dark mt-5">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center text-light py-3">
+    © 2020 Copyright: Silvia
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- End Footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ wisata.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/mine.css">
+    <title>Banjar</title>
+  </head>
+  <body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+  <a class="navbar-brand" href="#">
+    <img src="img/logo.png" width="70" height="40" class="d-inline-block align-top" alt="" loading="lazy">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="home.php">Beranda <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" href="wisata.php">Destinasi Wisata <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link" href="makan.php">Tempat Makan</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link" href="penginapan.php">Penginapan</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link" href="home.php#info">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- End Navbar -->
+
+<!-- Content -->
+  <div class="container-xl" id="content">
+    <div class="row">
+      <div class="col-lg-3">
+        <hr class="my-4">
+      </div>
+      <div class="col-lg-6">
+        <p class="text-center font-weight-bold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem sunt nisi voluptates facere enim quo? Sed saepe inventore sequi aliquid optio. Corrupti aspernatur totam ratione. Magnam animi facere illo magni!</p>
+      </div>
+      <div class="col-lg-3">
+        <hr class="my-4">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4">
+        <a href=""><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+        <div class="caption text-center">
+          <p>kakakaka</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <a href=""><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+        <div class="caption text-center">
+          <p>kakakaka</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <a href=""><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+        <div class="caption text-center">
+          <p>kakakaka</p>
+        </div>
+      </div>
+    
+      <div class="col-lg-4">
+        <a href=""><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+        <div class="caption text-center">
+          <p>kakakaka</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <a href=""><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+        <div class="caption text-center">
+          <p>kakakaka</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <a href=""><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+        <div class="caption text-center">
+          <p>kakakaka</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <a href=""><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+        <div class="caption text-center">
+          <p>kakakaka</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <a href=""><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+        <div class="caption text-center">
+          <p>kakakaka</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <a href=""><img src="img/MenaraPandang.jpg" class="foto" alt=""></a>
+        <div class="caption text-center">
+          <p>kakakaka</p>
+        </div>
+      </div>
+      </div>
+  </div>
+
+<!-- End Content -->
+
+<!-- Footer -->
+  <footer class="page-footer font-small bg-dark mt-5">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center text-light py-2">
+    © 2020 Copyright: Silvia
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- End Footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
